60ba2b197afbf9ad55df4171fa44bbd3c39f10c5
simple-dogood <59254270+simple-dogood@users.noreply.github.com>
2020-01-08 21:13:53 -0500

    ads

--- picks/models.py
+++ picks/models.py
@@ -24,13 +24,9 @@ class Choice(models.Model):
     team_selected = models.CharField(max_length=30)
     total_selected = models.CharField(max_length=10)
     created = models.DateTimeField(auto_now_add=True)
-    g_id = models.CharField(max_length=30,default=0)
+    g_id = str(self.player)+"_"+str(self.game)
 
-    @property
-    def combo(self):
-        return str(self.player)+"_"+str(self.game)
-
-    g_id = combo()
+    
 
 
 
