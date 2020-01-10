f277c4dabae7bc24328fc4631cb3ab4f6380488e
simple-dogood <59254270+simple-dogood@users.noreply.github.com>
2020-01-10 11:46:14 -0500

    id v1

--- picks/views.py
+++ picks/views.py
@@ -14,7 +14,7 @@ def view_games(request):
         form1 = ChoiceForm1(request.POST)
         if form1.is_valid():
             u = form1.save()
-            #r = Choice2.save(self)
+            Choice2.g_id.save()
             return HttpResponseRedirect('/picks/')
     else:
         form1 = ChoiceForm1()
