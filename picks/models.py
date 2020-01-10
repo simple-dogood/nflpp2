e238c33d067b75b2513d4263d7db9bd833d08dcf
simple-dogood <59254270+simple-dogood@users.noreply.github.com>
2020-01-10 11:55:20 -0500

    updated max length for spellin

--- picks/models.py
+++ picks/models.py
@@ -42,7 +42,7 @@ class Choice2(models.Model):
     team_selected = models.CharField(max_length=30)
     total_selected = models.CharField(max_length=10)
     created = models.DateTimeField(auto_now_add=True)
-    g_id = models.CharField(maxlegth=50)
+    g_id = models.CharField(maxlength=50)
 
     def save(self,*args,**kwargs):
         self.g_id = str(self.player)+"_"+str(self.game)
