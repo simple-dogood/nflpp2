3dbf3eef28e6957158121f927faca122a4a553a3
simple-dogood <59254270+simple-dogood@users.noreply.github.com>
2020-01-10 12:28:19 -0500

    created PickManager

--- picks/models.py
+++ picks/models.py
@@ -36,18 +36,7 @@ class Stand(models.Model):
     player = models.CharField(max_length=30)
     score = models.IntegerField()
 
-class Choice2(models.Model):
-    player = models.CharField(max_length = 30)
-    game = models.CharField(max_length=30)
-    team_selected = models.CharField(max_length=30)
-    total_selected = models.CharField(max_length=10)
-    created = models.DateTimeField(auto_now_add=True)
-    g_id = models.CharField(max_length=50)
-
-    def save(self,*args,**kwargs):
-        self.g_id = str(self.player)+"_"+str(self.game)
-        super(Choice2,self).save(*args, **kwargs)
-
+class PickManager(models.Manager):
     def last_pick(self):
         from django.db import connection
         with connection.cursor() as cursor:
@@ -59,6 +48,21 @@ class Choice2(models.Model):
             p = self.model(player=row[0],team_selected=row[1],total_selected=row[2],created=row[4])
             result_list.append(p)
             return result_list
+
+class Choice2(models.Model):
+    player = models.CharField(max_length = 30)
+    game = models.CharField(max_length=30)
+    team_selected = models.CharField(max_length=30)
+    total_selected = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+    g_id = models.CharField(max_length=50)
+    objects = PickManager()
+
+    def save(self,*args,**kwargs):
+        self.g_id = str(self.player)+"_"+str(self.game)
+        super(Choice2,self).save(*args, **kwargs)
+
+
 
 
 
