4c7a40730adfaf33ee78b4bea850b1bb7a7cc7c3
simple-dogood <59254270+simple-dogood@users.noreply.github.com>
2020-01-17 21:07:33 -0500

    W3 Updates

--- picks/forms.py
+++ picks/forms.py
@@ -7,7 +7,7 @@ from .models import Choice2
 players = (('Select Name','Select Name'),('Pex','Pex'),('Mac','Mac'),('Mullen','Mullen'),('Bdoc','Bdoc'),('Campbell','Campbell'),('Harbs','Harbs'),('Meat','Meat'),('Minas','Minas'),('Tony','Tony'))
 
 match_ups = (('Minnesota','Minnesota'),('San Fran','San Fran'),('Tennessee','Tennessee'),('Baltimore','Baltimore'),('Houston','Houston'),('Kansas City','Kansas City'),('Seattle','Seattle'),('Green Bay','Green Bay'),)
-match_ups = (('Clemson','Clemson'),('LSU','LSU'))
+
 
 pwk = (('CFB','CFB'),('N/A','N/A'))
 
